8e798ee52cc8874eafd4f9fdc48bd3f52f93c2d7
Jiangth <804770171@qq.com>
2021-11-12 13:55:59 +0800

    在线学习平台

--- service/service_edu/src/main/java/com/example/serviceedu/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/example/serviceedu/controller/EduTeacherController.java
@@ -1,6 +1,8 @@
 package com.example.serviceedu.controller;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.commonutils.R;
 import com.example.serviceedu.entity.EduTeacher;
 import com.example.serviceedu.service.EduTeacherService;
 import io.swagger.annotations.Api;
@@ -30,20 +32,34 @@ public class EduTeacherController {
     //1.查询讲师所有数据
     @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
-    public List<EduTeacher> findAllTeacher(){
+    public R findAllTeacher(){
         List<EduTeacher> list = eduTeacherService.list(null);
-        return list;
+        return R.ok().data("items",list);
     }
 
     //2.逻辑删除讲师的方法
     @ApiOperation(value = "根据ID删除讲师")
     @DeleteMapping("{id}")
-    public boolean removeTeacher(@PathVariable String id){
+    public R removeTeacher(@PathVariable String id){
         boolean b = eduTeacherService.removeById(id);
-        return b;
+        if(b){
+            return R.ok();
+        }
+        return R.error();
     }
 
-
+    //分页查询讲师的方法 current 当前页  limit 每页记录数
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current,
+                             @PathVariable long limit){
+        //创建配置对象
+        Page<EduTeacher> pageTeacher=new Page<>(1,3);
+        //调用方法的时候会将分页的所有数据封装到PageTeacher中
+        eduTeacherService.page(pageTeacher,null);
+        long total=pageTeacher.getTotal();
+        List<EduTeacher> list=pageTeacher.getRecords();
+        return R.ok().data("total",total).data("rows",list);
+    }
 
 
 
