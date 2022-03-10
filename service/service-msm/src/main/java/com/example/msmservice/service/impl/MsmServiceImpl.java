37584273c8a1d89e8e8c5f42afca372e3c36717c
jth1220 <65992786+jth1220@users.noreply.github.com>
2022-03-10 15:22:23 +0800

    Update MsmServiceImpl.java

--- service/service-msm/src/main/java/com/example/msmservice/service/impl/MsmServiceImpl.java
+++ service/service-msm/src/main/java/com/example/msmservice/service/impl/MsmServiceImpl.java
@@ -25,7 +25,7 @@ public class MsmServiceImpl implements MsmService {
              * 你也可以直接在代码中写死密钥对，但是小心不要将代码复制、上传或者分享给他人，
              * 以免泄露密钥对危及你的财产安全。
              * CAM密匙查询: https://console.cloud.tencent.com/cam/capi*/
-            Credential cred = new Credential("AKIDxkHZcOt75mCLDn69vWquiKfjW37uQfdK", "RcW9VaOu97pGQJvGK6csrZ1VjhPq1ziC");
+            Credential cred = new Credential("", "");
 
             // 实例化一个http选项，可选，没有特殊需求可以跳过
             HttpProfile httpProfile = new HttpProfile();
